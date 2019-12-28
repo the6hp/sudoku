529bdca890b641763df080176e6ffdd8d9dece6d
Aleksandr <www.al91@mail.ru>
2019-12-28 13:54:09 +0300

    Начало начал

--- playground_code_0001.playground/Contents.swift
+++ playground_code_0001.playground/Contents.swift
@@ -1,6 +1,4 @@
 var original_massiv: [Int] = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
 
 
-
-
 print(original_massiv)
---
+++ sudoku_v_0002/sudoku_v_0002/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sudoku_v_0002
+//
+//  Created by Alexander Penkov on 04.01.2020.
+//  Copyright © 2020 Alexander Penkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
