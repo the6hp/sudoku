d5758299bd8aaf0b839bd50d0067238ca03480eb
Aleksandr <www.al91@mail.ru>
2019-12-28 13:54:09 +0300

    Начало начал

---
+++ sudoku_v_0002/sudoku_v_0002/FillingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FillingViewController.swift
+//  sudoku_v_0002
+//
+//  Created by Alexander Penkov on 04.01.2020.
+//  Copyright © 2020 Alexander Penkov. All rights reserved.
+//
+
+import UIKit
+
+class FillingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
