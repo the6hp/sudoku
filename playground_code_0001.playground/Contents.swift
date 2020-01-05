f16a995119abf9352e26e93bed1e7c52293f2ea3
the6hp <40247885+the6hp@users.noreply.github.com>
2020-01-05 04:39:57 +0300

    Revert "Playground"

--- playground_code_0001.playground/Contents.swift
+++ playground_code_0001.playground/Contents.swift
@@ -1,17 +1,4 @@
-    var allNumbers = [1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,1,2,3,7,8,9,1,2,3,4,5,6,2,3,4,5,6,7,8,9,1,5,6,7,8,9,1,2,3,4,8,9,1,2,3,4,5,6,7,3,4,5,6,7,8,9,1,2,6,7,8,9,1,2,3,4,5,9,1,2,3,4,5,6,7,8]
+var original_massiv: [Int] = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
 
-    
-    func swap (array: [Int]) -> [String]  {
-    print(array)
-    var originalMassiv = [String]()
-        
-        var i = 0
-        repeat {
-            var a = String(array[i])
-            originalMassiv.append(a)
-            i += 1
-        } while i < array.count
-        
-        
 
 print(original_massiv)
