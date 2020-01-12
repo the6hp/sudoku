4b3757efb904ffa3e6785d73bc7544ab036c8613
Aleksandr <www.al91@mail.ru>
2020-01-12 17:10:53 +0300

    statistic

--- uicollectionView_lesson/uicollectionView_lesson/controller/StatisticViewController.swift
+++ uicollectionView_lesson/uicollectionView_lesson/controller/StatisticViewController.swift
@@ -12,19 +12,103 @@ class StatisticViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeBorder()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var gamesPlayedLabel: UILabel!
+    @IBOutlet weak var gamesCompletedLabel: UILabel!
+    @IBOutlet weak var winRateLabel: UILabel!
+    @IBOutlet weak var betWinStreakLabel: UILabel!
+    @IBOutlet weak var bestTimeLabel: UILabel!
+    @IBOutlet weak var averageTimeLabel: UILabel!
+    
+    
+    @IBOutlet weak var gamesPlayedLabelText: UILabel!
+    @IBOutlet weak var gamesCompletedLabelText: UILabel!
+    @IBOutlet weak var winRateLabelText: UILabel!
+    @IBOutlet weak var bestWinStreakLabelText: UILabel!
+    @IBOutlet weak var bestTimeLabelText: UILabel!
+    @IBOutlet weak var averageTimeLabelText: UILabel!
+    
+    
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBAction func changeIndex(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            gamesPlayedLabelText.text = "0"
+            gamesCompletedLabelText.text = "0"
+            winRateLabelText.text = "0"
+            bestWinStreakLabelText.text = "0"
+            bestTimeLabelText.text = "00:00"
+            averageTimeLabelText.text = "00:00"
+        case 1:
+            gamesPlayedLabelText.text = "1"
+            gamesCompletedLabelText.text = "1"
+            winRateLabelText.text = "1"
+            bestWinStreakLabelText.text = "1"
+            bestTimeLabelText.text = "01:01"
+            averageTimeLabelText.text = "10:10"
+        case 2:
+            gamesPlayedLabelText.text = "2"
+            gamesCompletedLabelText.text = "2"
+            winRateLabelText.text = "2"
+            bestWinStreakLabelText.text = "2"
+            bestTimeLabelText.text = "02:02"
+            averageTimeLabelText.text = "20:20"
+        case 3:
+            gamesPlayedLabelText.text = "3"
+            gamesCompletedLabelText.text = "3"
+            winRateLabelText.text = "3"
+            bestWinStreakLabelText.text = "3"
+            bestTimeLabelText.text = "03:03"
+            averageTimeLabelText.text = "30:30"
+        default:
+            break
+        }
     }
-    */
+    
+    
+    func changeBorder () {
+        gamesPlayedLabel.layer.addBorder(edge: UIRectEdge.top, color: UIColor.black, thickness: 0.3)
+        gamesPlayedLabel.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.black, thickness: 0.3)
+        gamesCompletedLabel.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.black, thickness: 0.3)
+        winRateLabel.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.black, thickness: 0.3)
+        betWinStreakLabel.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.black, thickness: 0.3)
+        bestTimeLabel.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.black, thickness: 0.3)
+        averageTimeLabel.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.black, thickness: 0.3)
 
+    }
+    
+    
+    
+}
+
+
+extension CALayer {
+    func addBorder(edge: UIRectEdge, color: UIColor, thickness: CGFloat) {
+
+        let border = CALayer()
+
+        switch edge {
+            case UIRectEdge.top:
+             border.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: thickness)
+
+            case UIRectEdge.bottom:
+             border.frame = CGRect(x: 0, y: self.bounds.height - thickness,  width: self.bounds.width, height: thickness)
+
+            case UIRectEdge.left:
+             border.frame = CGRect(x: 0, y: 0,  width: thickness, height: self.bounds.height)
+
+            case UIRectEdge.right:
+             border.frame = CGRect(x: self.bounds.width - thickness, y: 0,  width: thickness, height: self.bounds.height)
+
+            default:
+             break
+        }
+        border.backgroundColor = color.cgColor;
+        self.addSublayer(border)
+    }
 }
